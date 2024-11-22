2f836f6bb60fb4f840f59079e4be399f7adca30c
mehmetay10 <mhmt9897@outlook.com>
2024-11-22 18:59:17 +0300

    First Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,31 @@
+import Categories from "./components/Categories/Categories";
+import Footer from "./components/Layout/Footer/Footer";
+import Header from "./components/Layout/Header/Header";
+import Policy from "./components/Layout/Policy/Policy";
+import Sliders from "./components/Layout/Slider/Sliders";
+import Products from "./components/Products/Products";
+import "./App.css";
+import Campaigns from "./components/Campaigns/Campaigns";
+import Blogs from "./components/Blogs/Blogs";
+import Brands from "./components/Brands/Brands";
+import CampaignSingle from "./components/CampaignSingle/CampaignSingle";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Sliders />
+      <Categories />
+      <Products />
+      <Campaigns />
+      <Products />
+      <Blogs />
+      <Brands />
+      <CampaignSingle />
+      <Policy />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
