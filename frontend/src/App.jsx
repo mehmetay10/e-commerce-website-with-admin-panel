9ef3c67fb17470c4818fec94b8f8201c220d210b
mehmetay10 <mhmt9897@outlook.com>
2024-11-25 17:09:55 +0300

    add shoppage-contactpage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
-import HomePage from "./pages/HomePage";
 import React from "react";
+// import HomePage from "./pages/HomePage";
+// import ShopPage from "./pages/ShopPage";
 import "./App.css";
+import ContactPage from "./pages/ContactPage";
 
 function App() {
   return (
     <React.Fragment>
-      <HomePage />
+      {/* <HomePage /> */}
+      {/* <ShopPage /> */}
+      <ContactPage />
     </React.Fragment>
   );
 }
